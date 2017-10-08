5c0e69ef67489f16da80cc43ac11f43cc4c5525f
David Clark <davidclark314@gmail.com>
2017-10-08 11:59:32 -0600

    I've defined most of the necessary back-end logic for the Data Store into a WordStore class.

---
+++ WordStore.class.js
@@ -0,0 +1,81 @@
+export default class WordStore {
+	constructor() {
+		this.anagramMap = {}  // Map of words with values that are Sets of anagrams for that word
+		this.sizeMap = {}  // Map of sizes (length of word) with values that are Sets of words of matching size
+	}
+
+	getAnagrams(word: string, limit: number? = null): string[] {
+		/* word: word to get anagrams for
+		 * limit: how many words to return (default null - return all)
+		 * return: array of anagrams for that word
+		*/
+
+		word = word.toLowerCase()
+		const anagramSet = this.anagramMap.word
+
+		if(!anagramSet) return []  // No entry for that word
+
+		const anagramList = [...anagramSet]
+
+		if(limit) return anagramList.slice(0, limit)  // Had an entry but need to limit results
+		return anagramList  // Had an entry and no need to limit results
+	}
+
+	addWords(words: string[]) {
+		words.forEach(word => {
+			word = word.toLowerCase()
+			if(this.anagramMap.word) return  // If we already have this word, skip it
+
+			// Don't have the word, so add it
+			this.anagramMap.word = new Set()
+			// We may or may not already have entries of this size, so init if not
+			if(!this.sizeMap[word.length]) this.sizeMap[word.length] = new Set()
+
+			// Grab words of same size before adding new word (a word is not its own anagram)
+			const sameSizeWords = [...this.sizeMap[word.length]]
+
+			// Now add the word to the size Set
+			this.sizeMap[word.length].add(word)
+
+			// Get all anagrams for the new word and populate its anagramMap
+			sameSizeWords.forEach(sameSizeWord => {
+				if(areAnagrams(word, sameSizeWord)) {
+					this.anagramMap.word.add(sameSizeWord)
+				}
+			})
+
+			// For each word found as an anagram, we need to add the new word to their lists
+			[...this.anagramMap.word].forEach(anagram => {
+				this.anagramMap.anagram.add(word)
+			})
+		})
+	}
+
+	deleteWord(word: string) {
+		word = word.toLowerCase()
+		// If we don't have the word, nothing to do
+		if(!this.anagramMap.word) return
+
+		// Remove the word from the sizeMap
+		this.sizeMap[word.length].delete(word)
+
+		// To remove from the anagramMap, we need to remove the word's own entry
+		// and also remove it from its anagrams' lists
+		const anagrams = [...this.anagramMap.word]
+		delete this.anagramMap.word
+		anagrams.forEach(anagram => {
+			this.anagramMap.anagram.delete(word)
+		})
+	}
+
+	deleteAll() {
+		this.anagramMap = {}
+		this.sizeMap = {}
+	}
+
+	// TODO: Implement this
+	areAnagrams(word1: string, word2: string): boolean {
+		// Returns whether word1 and word2 are anagrams of each other
+		return false
+	}
+}
